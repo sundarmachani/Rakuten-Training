43420ec1b45310ab7c5c1efda12bdb501592ce7f
sundar-machani <machani.sundar@rakuten.com>
2022-04-01 17:19:48 +0530

    first commit

---
+++ web1/webContent/app.js
@@ -0,0 +1,9 @@
+function Save() {
+	const element = document.getElementById('firstname');
+	console.log(element.value);
+	const element1 = document.getElementById('age');
+	console.log(element1.value);
+		const element2 = document.getElementById('input');
+	console.log(element2.value);
+	
+}
